b1029c4003e12fe5092c4d173386e881bfe3bf64
lolloluvyoxx-maker <lolloluvyoxx@gmail.com>
2026-01-09 19:46:22 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+// 🔴 METTI QUI GLI ID
+const ROLE_ID = "ID_DEL_RUOLO";
+const GUILD_ID = "ID_DEL_SERVER";
+
+client.once("ready", () => {
+  console.log(`Online come ${client.user.tag}`);
+});
+
+client.on("guildMemberUpdate", async (oldMember, newMember) => {
+  const before = oldMember.premiumSince;
+  const after = newMember.premiumSince;
+
+  // quando boosta
+  if (!before && after) {
+    await newMember.roles.add(ROLE_ID).catch(console.error);
+  }
+
+  // quando toglie il boost
+  if (before && !after) {
+    await newMember.roles.remove(ROLE_ID).catch(console.error);
+  }
+});
+
+client.login(process.env.TOKEN);
